e7ff6537939666871ecd5bc5039759ae7a98977e
lekhanhlinh-star <82929088+lekhanhlinh-star@users.noreply.github.com>
2023-04-25 17:38:11 +0700

    build models

--- laptopsite/CustomerSite/models.py
+++ laptopsite/CustomerSite/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Laptop(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
+
+class Customer(User):
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('C', 'Complete'),
+        ('R', 'Refunded'),
+    ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.username} - {self.laptop.name}"
+
+    def total_price(self):
+        return self.quantity * self.laptop.price
+class Transaction(models.Model):
+    STATUS_CHOICES = [
+        ('S', 'Success'),
+        ('F', 'Failed'),
+    ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.user.username} - {self.order.laptop.name}"
